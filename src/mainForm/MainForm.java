2ee099ff5116c03808c84d763923ba42a0b0e0ea
Yura Trambitskiy <tr.yura@yahoo.com>
2015-02-23 22:59:42 +0300

    centered main window

--- src/mainForm/MainForm.java
+++ src/mainForm/MainForm.java
@@ -33,6 +33,10 @@ public class MainForm extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //centering window
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+
         //configuring mediator
         mediator = new FormMediator(this);
         fileLabel.setName("fileLabel");
